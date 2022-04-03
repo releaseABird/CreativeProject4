e5673b84fe09f95f3ecb41137d52c3b668b9af5e
Lisa Bird <misademoss24@gmail.com>
2022-04-03 15:42:21 -0700

    Finished rule page

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -17,7 +17,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/contribute',
+    name: 'contribute',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ContributeView.vue')
+  },
+  {
+    path: '/',
+    name: 'sketchbook',
+    component: () => import(/* webpackChunkName: "about" */ '../views/SketchbookView.vue')
+  },
 ]
 
 const router = new VueRouter({
--- front-end/src/views/AboutView.vue
+++ front-end/src/views/AboutView.vue
@@ -8,13 +8,27 @@
     <h2>Steps</h2>
     <div class="steps">
       <div class="step">
-        <p>1. Make your BINGO card. Draw a 5x5 grid and label the columns with BINGO. For each column, use any number between 1 and 20 inclusive. You can repeat the same number if it is in a different column (ie B15 and I15), but you cannot have
-          multiples of the number in the same column(ie you cannot have a column of G3s)</p>
+        <p>1. Make your BINGO card. Draw a 5x5 grid and label the columns with BINGO. For each column, use any number between 1 and 20 inclusive.<br> You can repeat the same number if it is in a different column (ie B15 and I15), but you cannot have
+          multiples of the number in the same column(ie you cannot have a column of G3s).<br>BONUS: come up with a way to treat yourself for when you get bingo</p>
         <img class="step-pic" src="../assets/step1.png">
       </div>
+      <br>
       <div class="step">
-      <p>2. Click on the "BINGO" option in the menu, and get your first challenge by clicking on the orange face. Your prompt should appear in the light brown box below it.</p>
+      <p>2. Click on the "BINGO" option in the menu, and get your first challenge by clicking on the orange face.<br> Your prompt should appear in the light brown box below it.
+      If you don't like the prompt, you can click the face again until you get a challenge you like. However, sometimes it is nice to go outside your comfort zone!</p>
       <img class="step-pic" src="../assets/step2.png">
+      </div>
+      <br>
+      <div class ="step">
+      <p>3. Do your best to accomplish the challenge and have the most fun you can with it. <br> It does not have to be perfect. It's called a SKETCHbook, and not a perfectly-fully-rendered-book, for a reason.<br>Spend as much time as you would like. I wouldn't spend more than a day on the challenge. </p>
+      <img class="step-pic" src="../assets/step3.png">
+      </div>
+      <br>
+      <div class="step">
+      <p>4. After you finish your challenge, click on the pink button below the light brown box to reveal the bingo letter you received. <br>You should receive a letter and number that follows the same parameters as your bingo card. <br>
+      If you are playing on easy mode, you can keep pressing the pink button until you get something on your card. Otherwise, you can get back to drawing. <br>
+      You can do one challenge a day or repeat steps 2-4 until you get a BINGO! If you get a BINGO, you should treat yourself.</p>
+      <img class="step-pic" src="../assets/button2-norm.png">
       </div>
     </div>
   </div>
